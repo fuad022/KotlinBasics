e1d460d0f8e986fbe0905b689c000811ff541e1d
f.r.malikov <fuad.malikov@rabitabank.com>
2023-04-25 17:01:28 +0400

    Update.

---
+++ app/src/main/java/com/example/kotlinbasics/Atomicity.kt
@@ -0,0 +1,29 @@
+package com.example.kotlinbasics
+
+import java.util.concurrent.atomic.AtomicInteger
+
+fun main() {
+    val counter = AtomicInteger(0)
+
+    // Создаем 10 потоков, каждый из которых инкрементирует счетчик 1000 раз
+    val threads = List(10) {
+        Thread {
+            for (i in 1..1000) {
+                counter.incrementAndGet()
+            }
+        }
+    }
+
+    // Запускаем потоки
+    threads.forEach {
+        it.start()
+    }
+
+    // Ждем завершения потоков
+    threads.forEach {
+        it.join()
+    }
+
+    // Выводим результат
+    println("Counter value: ${counter.get()}")
+}
