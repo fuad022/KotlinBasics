f839fe46a7bd3138948cfc2f03e516440e0dff85
fuad022 <fuad-022@mail.ru>
2023-05-21 16:10:19 +0400

    Minor changes.

--- app/src/main/java/com/example/kotlinbasics/Example3.kt
+++ app/src/main/java/com/example/kotlinbasics/Example3.kt
@@ -2,9 +2,26 @@ package com.example.kotlinbasics
 
 fun main() {
 //    println(groupWords(arrayOf("eat", "tea", "tan", "ate", "nat", "bat")))
+//    println("Test message")
 
-    println("Test message")
+    val objects = listOf(
+        MyObject(1, true),
+        MyObject(2, false),
+        MyObject(3, true)
+    )
+
+    val people = Persons("Alice", 25, objects)
+
+    if (people.objects.filter { it.isActive }.size > 1) {
+        println("Objects list more 1 size isActive")
+    } else {
+        println("Objects list no has isActive")
+    }
 }
+
+data class Persons(val name: String, val age: Int, val objects: List<MyObject>)
+data class MyObject(val id: Int, val isActive: Boolean)
+
 
 fun groupWords(words: Array<String>): List<List<String>> {
     val result: MutableList<List<String>> = mutableListOf()
