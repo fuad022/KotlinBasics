be9838c5829992c2501c85cbb47904c2a7503c5e
f.r.malikov <fuad.malikov@rabitabank.com>
2023-07-31 09:39:47 +0400

    Update.

--- app/src/main/java/com/example/kotlinbasics/Example2.kt
+++ app/src/main/java/com/example/kotlinbasics/Example2.kt
@@ -2,6 +2,14 @@ package com.example.kotlinbasics
 
 fun main() {
     println(countLetters("AAAAABBBBBCCXYZDDDDDEEEEEFFFFAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"))
+
+    val arr = arrayOfNulls<Int>(5)
+    arr[0] = 1
+    arr[1] = 2
+    arr[2] = 3
+    arr[3] = 4
+    arr[4] = 5
+    println(arr.contentToString())
 }
 
 fun countLetters(str: String): String {
